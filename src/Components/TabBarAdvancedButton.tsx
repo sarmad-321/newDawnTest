0a823a1bd9acd04729044b7d6be1729128007735
wardahFatimaSalsoft4071 <wardah.fatima@salsoft.net>
2023-06-28 14:17:44 +0500

    ios fixes

--- src/Components/TabBarAdvancedButton.tsx
+++ src/Components/TabBarAdvancedButton.tsx
@@ -1,63 +1,50 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, View, Image } from 'react-native';
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs/lib/typescript/src/types';
-import { TabBg } from './TabBg';
+import {StyleSheet, TouchableOpacity, View, Image} from 'react-native';
+import {BottomTabBarButtonProps} from '@react-navigation/bottom-tabs/lib/typescript/src/types';
+import {TabBg} from './TabBg';
 import LinearGradient from 'react-native-linear-gradient';
-import { icons } from '../Assets';
-import { vh } from '../Utils/units';
-import { Circle } from 'react-native-svg';
-
-
-
-
+import {icons} from '../Assets';
+import {vh} from '../Utils/units';
+import {Circle} from 'react-native-svg';
 
 type Props = BottomTabBarButtonProps & {
-    bgColor?: string;
+  bgColor?: string;
 };
 
-export const TabBarAdvancedButton: React.FC<Props> = ({
-    bgColor,
-    ...props
-}) => (
-    <View
-        style={styles.container}
-        pointerEvents="box-none"
-    >
-        <TabBg
-            color={bgColor}
-            style={styles.background}
-        />
+export const TabBarAdvancedButton: React.FC<Props> = ({bgColor, ...props}) => (
+  <View style={styles.container} pointerEvents="box-none">
+    <TabBg color={bgColor} style={styles.background} />
 
-        <LinearGradient
-            colors={['#97A3E2', '#3E59E8']}
-            style={styles.button}
-        >
-            <Image source={icons.plus} style={{ resizeMode: "contain", height: "40%", width: "40%" }} />
-        </LinearGradient>
-    </View>
+    <LinearGradient colors={['#97A3E2', '#3E59E8']} style={styles.button}>
+      <Image
+        source={icons.plus}
+        style={{resizeMode: 'contain', height: '40%', width: '40%'}}
+      />
+    </LinearGradient>
+  </View>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        position: 'relative',
-        width: 75,
-        alignItems: 'center'
-    },
-    background: {
-        position: 'absolute',
-        top: 0,
-    },
-    button: {
-        top: -22.5,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: vh * 6,
-        height: vh * 6,
-        borderRadius: 27,
-        // backgroundColor: 'blue',
-    },
-    buttonIcon: {
-        fontSize: 16,
-        color: '#F6F7EB'
-    }
+  container: {
+    position: 'relative',
+    width: 75,
+    alignItems: 'center',
+  },
+  background: {
+    position: 'absolute',
+    top: 0,
+  },
+  button: {
+    top: -22.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: vh * 6,
+    height: vh * 6,
+    borderRadius: 27,
+    // backgroundColor: 'blue',
+  },
+  buttonIcon: {
+    fontSize: 16,
+    color: '#F6F7EB',
+  },
 });
--- src/Navigations/BottomTabNavigator.js
+++ src/Navigations/BottomTabNavigator.js
@@ -1,192 +1,154 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { BottomTabBar, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {
+  BottomTabBar,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screens/HomeScreen';
-import { icons } from '../Assets';
-import { NavigationContainer } from '@react-navigation/native';
-import { vh, vw } from '../Utils/units';
-import { TabBarAdvancedButton } from '../Components/TabBarAdvancedButton';
+import {icons} from '../Assets';
+import {NavigationContainer} from '@react-navigation/native';
+import {vh, vw} from '../Utils/units';
+import {TabBarAdvancedButton} from '../Components/TabBarAdvancedButton';
 
 const BottomTab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
-    return (
-        <NavigationContainer>
-            <BottomTab.Navigator
+  return (
+    <NavigationContainer>
+      <BottomTab.Navigator
+        tabBar={props => (
+          <View style={styles.navigatorContainer}>
+            <BottomTabBar {...props} />
+          </View>
+        )}
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: false,
+          tabBarShowLabel: false,
+          tabBarHideOnKeyboard: true,
+          tabBarItemStyle: {
+            backgroundColor: '#F6F7EB',
+            height: vh * 7.2,
+            justifyContent: 'flex-start',
+          },
+          tabBarStyle: styles.navigator,
+        }}>
+        <BottomTab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={[styles.tabButton]}>
+                <Image source={icons.home} style={styles.tabActiveStyle} />
+              </View>
+            ),
+          }}
+        />
 
-                tabBar={(props) => (
-                    <View style={styles.navigatorContainer}>
-                        <BottomTabBar
-                            {...props}
-                        />
+        <BottomTab.Screen
+          name="Search"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={[styles.tabButton]}>
+                <Image source={icons.search} style={styles.tabActiveStyle} />
+              </View>
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="Create"
+          component={HomeScreen}
+          options={{
+            tabBarButton: props => (
+              <View style={[styles.tabButton]}>
+                <TabBarAdvancedButton bgColor={'#F6F7EB'} {...props} />
+              </View>
+            ),
+          }}
+        />
 
-                    </View>
-                )}
+        <BottomTab.Screen
+          name="Cart"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={[styles.tabButton]}>
+                <Image source={icons.cart} style={styles.tabActiveStyle} />
+              </View>
+            ),
+          }}
+        />
 
-                screenOptions={{
+        <BottomTab.Screen
+          name="Gifts"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={[styles.tabButton]}>
+                <Image source={icons.gift} style={styles.tabActiveStyle} />
+              </View>
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
+};
 
-                    headerShown: false,
-                    gestureEnabled: false,
-                    tabBarShowLabel: false,
-                    tabBarHideOnKeyboard: true,
-                    tabBarItemStyle: {
-                        backgroundColor: "#F6F7EB"
-                    },
-                    tabBarStyle: styles.navigator,
-                }}>
-                <BottomTab.Screen
-                    name="Home"
-                    component={HomeScreen}
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View style={[styles.tabButton]}>
-                                <Image
-                                    source={icons.home}
-                                    style={styles.tabActiveStyle}
-                                />
-
-                            </View>
-                        ),
-                    }}
-                />
-
-                <BottomTab.Screen
-                    name="Search"
-                    component={HomeScreen}
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View style={[styles.tabButton]}>
-                                <Image
-                                    source={icons.search}
-                                    style={styles.tabActiveStyle}
-
-                                />
-                            </View>
-                        ),
-                    }}
-                />
-                <BottomTab.Screen
-                    name="Create"
-                    component={HomeScreen}
-                    options={{
-                        tabBarButton: (props) => (
-                            <TabBarAdvancedButton
-                                bgColor={'#F6F7EB'}
-                                {...props}
-                            />
-                        )
-
-                    }}
-                />
-
-                <BottomTab.Screen
-                    name="Cart"
-                    component={HomeScreen}
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View style={[styles.tabButton]}>
-                                <Image
-                                    source={icons.cart}
-                                    style={styles.tabActiveStyle}
-
-                                />
-
-                            </View>
-                        ),
-                    }}
-                />
-
-                <BottomTab.Screen
-                    name="Gifts"
-                    component={HomeScreen}
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View style={[styles.tabButton]}>
-                                <Image
-                                    source={icons.gift}
-                                    style={styles.tabActiveStyle}
-
-                                />
-
-                            </View>
-                        ),
-                    }}
-                />
-            </BottomTab.Navigator>
-        </NavigationContainer>
-    )
-}
-
-
-export default BottomTabNavigator
+export default BottomTabNavigator;
 
 const styles = StyleSheet.create({
-    // navigatorContainer: {
-    //     position: 'absolute',
-    //     bottom: 0,
-    //     left: 0,
-    //     right: 0,
-    //     backgroundColor: "transparent",
-    //     // SHADOW
-    //     // shadowColor: "#000",
-    //     // shadowOffset: {
-    //     //     width: 0,
-    //     //     height: 1,
-    //     // },
-    //     // shadowOpacity: 0.22,
-    //     // shadowRadius: 2.22,
-    // },
-    // tabStyle: {
-    //     height: vh * 10,
-    //     backgroundColor: "transparent"
-    //     // backgroundColor: "white",
-    // },
-    // tabButton: {
-    //     alignItems: 'center',
-    //     justifyContent: 'space-evenly',
-    //     position: 'absolute',
-    //     width: '100%',
-    //     height: vh * 7,
-    // },
-    tabActiveStyle: {
-        // marginTop: vh,
-        width: vh * 4.2,
-        height: vh * 4.2,
-        resizeMode: 'contain',
-    },
-    // createButton: {
-    //     // height: vh * 8,
-    //     // width: vh * 8,
-    //     // backgroundColor: "red",
-    //     borderRadius: vh * 100
-    // },
-    container: {
-        flex: 1
-    },
-    navigatorContainer: {
-        position: 'absolute',
-
-        bottom: 0,
-        left: 0,
-        right: 0,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.22,
-        shadowRadius: 2.22,
-    },
-    navigator: {
-        borderTopWidth: 0,
-        backgroundColor: 'transparent',
-        elevation: 30
-    },
-    xFillLine: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        height: 34
-    }
-})
+  // navigatorContainer: {
+  //     position: 'absolute',
+  //     bottom: 0,
+  //     left: 0,
+  //     right: 0,
+  //     backgroundColor: "transparent",
+  //     // SHADOW
+  //     // shadowColor: "#000",
+  //     // shadowOffset: {
+  //     //     width: 0,
+  //     //     height: 1,
+  //     // },
+  //     // shadowOpacity: 0.22,
+  //     // shadowRadius: 2.22,
+  // },
+  // tabStyle: {
+  //     height: vh * 10,
+  //     backgroundColor: "transparent"
+  //     // backgroundColor: "white",
+  // },
+  // tabButton: {
+  //     alignItems: 'center',
+  //     justifyContent: 'space-evenly',
+  //     position: 'absolute',
+  //     width: '100%',
+  //     height: vh * 7,
+  // },
+  tabActiveStyle: {
+    width: vh * 3,
+    height: vh * 3,
+    resizeMode: 'contain',
+  },
+  // createButton: {
+  //     // height: vh * 8,
+  //     // width: vh * 8,
+  //     // backgroundColor: "red",
+  //     borderRadius: vh * 100
+  // },
+  container: {
+    flex: 1,
+  },
+  navigatorContainer: {
+    position: 'absolute',
+    bottom: -vh * 2,
+    left: 0,
+    right: 0,
+  },
+  navigator: {
+    borderTopWidth: 0,
+    backgroundColor: 'transparent',
+    // elevation: 30,
+  },
+});
